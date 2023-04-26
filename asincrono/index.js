8ee51623048fe5beaba7b1a80fa5627fc33cff63
marianorigo18 <marianowagnerrigo@gmail.com>
2023-04-26 18:57:14 -0300

    fix commit

---
+++ asincrono/index.js
@@ -0,0 +1,9 @@
+//codigo asincrono no siempre se va a ejecutar en un orden 
+//de arriba hacia abajo
+
+//codigo asincrono se sale de la linea de ejecucion normal
+//sin para el codigo sincrono
+
+function saludar(){
+    console.log("Hello World")
+}
